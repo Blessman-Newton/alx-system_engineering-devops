7234949b534098d36c8058235c504456bc333bd1
Blessman-Newton <blessmannewton0@gmail.com>
2023-11-15 13:15:16 +0000

    file updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Manifest file: install_flask.pp
+
+package { 'python3-pip':
+  ensure => installed,
 }
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
+}
+'
